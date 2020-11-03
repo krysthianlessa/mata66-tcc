85c33b02956787c98037616ae88e5b3cff982e1e
Krysthian Lessa <krysthianlessa@gmail.com>
2020-11-02 21:07:35 -0300

    Adiciona main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from rake_nltk import Rake
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+
+df = pd.read_csv('https://query.data.world/s/uikepcpffyo2nhig52xxeevdialfl7')
+
+df = df[['Title','Genre','Director','Actors','Plot']]
+df.head()
